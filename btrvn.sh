f8609a17f30e1e0b7916f0d150648c49c460af33
mblanka23 <144958444+mblanka23@users.noreply.github.com>
2023-09-19 17:25:24 +1000

    btrvn.sh

--- btrvn.sh
+++ btrvn.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-cd /tmp && wget https://github.com/develsoftware/GMinerRelease/releases/download/3.41/gminer_3_41_linux64.tar.xz && tar -xvf gminer_3_41_linux64.tar.xz && clear && read -p "YOUR WALLET ADDRESS: " wallet_address && ./miner --algo kawpow --server rvn.2miners.com:6060 --user $wallet_address.rig_madrid
+cd /tmp && wget https://github.com/develsoftware/GMinerRelease/releases/download/3.41/gminer_3_41_linux64.tar.xz && tar -xvf gminer_3_41_linux64.tar.xz && clear && read -p "YOUR WALLET ADDRESS: " wallet_address && ./miner --algo kawpow --server rvn.2miners.com:6060 --user $wallet_address.rig_harjuma
